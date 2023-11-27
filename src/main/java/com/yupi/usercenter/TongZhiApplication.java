2260f979f8e32c971d9e832d281696d9ce5fef64
wy <2511933030@qq.com>
2023-11-28 00:47:10 +0800

    tongzhi init

--- src/main/java/com/yupi/usercenter/TongZhiApplication.java
+++ src/main/java/com/yupi/usercenter/TongZhiApplication.java
@@ -5,14 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * 启动类
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @SpringBootApplication
 @MapperScan("com.yupi.usercenter.mapper")
